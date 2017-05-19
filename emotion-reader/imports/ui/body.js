3e96fb4fd83a8aedf7ff52bffbfbfeeb5ca0634f
Maria Silla <maria.sylla@gmail.com>
2017-05-18 23:42:14 -0400

    add watson emotion api

--- emotion-reader/imports/ui/body.js
+++ emotion-reader/imports/ui/body.js
@@ -3,7 +3,12 @@ import {Template} from 'meteor/templating';
 import {Items} from '../api/items.js';
 
 import './body.html';
+// var Chart = require('chart.js')
 
+
+var postTitle;
+var postText;
+var giphUrl;
 
 //helper-
 //modify the body template - object
@@ -23,40 +28,305 @@ Template.body.events({
     console.log('hello');
   }
 })
-
+//render posts array to page
 Template.stream.posts = function () {
     return Posts.find({}, {sort: {published: -1}});
 };
 
+//on click insert into mongo db
 Template.editor.events({
     "click #submit-post": function() {
       var published_date = new Date();
+      postTitle = document.getElementById("title-area").value;
+      postText = document.getElementById("editor-area").value;
+
+ //Giphy API call
+      $.ajax({
+       url: 'http://api.giphy.com/v1/gifs/search?q=' + postTitle + '&api_key=dc6zaTOxFJmzC',
+       dataType: 'json',
+       success: function(response) {
+         // console.log(response.data);
+         var resArr = response.data;
+         var randomItem = resArr[Math.floor(Math.random()*resArr.length)];
+
+         giphUrl = randomItem.images.downsized_small.mp4;
+         console.log(giphUrl);
+
+         }
+       });
+ //Giphy API call ends here
+
       Posts.insert({
-        title: document.getElementById("title-area").value,
-        text: document.getElementById("editor-area").value,
+        title: postTitle,
+        text: postText,
         published: published_date.toLocaleTimeString()
       })
+      // $("#giphImg").attr('src',giphUrl);
     }
   });
 
+//Delete posts
 Template.post.events({
     "click .remove": function () {
       Posts.remove(this._id);
     }
 });
 
+//Watson API AJAX call
 
 
 
-//Random User
-$.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
 
+// $.ajax({
+//           method: "POST",
+//           version: 'v2-beta',
+//           url: "https://gateway.watsonplatform.net/natural-language-understanding/api",
+//           username: "446d06fa-2fca-43c2-b645-cdb0e75456ad",
+//           password: "n2tf1iPz32oO",
+//           version_date:'2015-12-02',
+//           natural_language_understanding: [
+//               {
+//               name: "natural-language-understanding",
+//               label: "natural-language-understanding",
+//               plan: "free",
+//               credentials: {
+//                  url: "https://gateway.watsonplatform.net/natural-language-understanding/api",
+//                  username: "446d06fa-2fca-43c2-b645-cdb0e75456ad",
+//                  password: "n2tf1iPz32oO"
+//                 }
+//               }
+//             ]
+//           }).done(function(msg){
+//           if (200) {
+//             console.log("This is a test for if.")
+//           } else {
+//             console.log("This is a test for else.")
+//           }
+//         });
+
+
+
+        // //Random User
+        // $.ajax({
+        //   url: 'https://randomuser.me/api/',
+        //   dataType: 'json',
+        //   success: function(data) {
+        //     console.log(data);
+        //   }
+        // });
+
+
+
+
+
+//Add Chart.js CDN
+
+// Template.body.onRendered(function () {
+//        console.log("hello!!!!!!");
+//       $('head').append('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>');
+//
+// });
+
+
+//Watson API starts here
+// {
+//   "url": "https://gateway.watsonplatform.net/natural-language-understanding/api",
+//   "username": "446d06fa-2fca-43c2-b645-cdb0e75456ad",
+//   "password": "n2tf1iPz32oO"
+// }
+
+//
+// var piechart = document.getElementById("myChart").getContext("2d");
+// var myPieChart = new Chart(piechart,{
+//     type: 'pie',
+//     data:  {
+//     labels: [
+//         "Red",
+//         "Blue",
+//         "Yellow"
+//     ],
+//     datasets: [
+//         {
+//             data: [300, 50, 100],
+//             backgroundColor: [
+//                 "#FF6384",
+//                 "#36A2EB",
+//                 "#FFCE56"
+//             ],
+//             hoverBackgroundColor: [
+//                 "#FF6384",
+//                 "#36A2EB",
+//                 "#FFCE56"
+//             ]
+//         }]
+// },
+//    options: {
+//         responsive: true
+//     }
+// });
+
+// document.addEventListener( "DOMContentLoaded", bindButtons );
+//
+// function bindButtons(  ){
+// 	document.getElementById( "submit" ).addEventListener( "click", function( event ){
+// 		var req = new XMLHttpRequest();
+// 		var sourceURL = encodeURIComponent(document.getElementById( "source" ).value);
+//
+// 		var endpoint = "https://gateway-a.watsonplatform.net/calls/url/URLGetEmotion?";
+// 		var apikey = "your-api-key";
+// 		var outputMode = "json";
+// 		var sourceText = "cleaned_or_raw";
+// 		var showSourceText = "1";
+//
+// 		req.open("GET", endpoint + "url=" + sourceURL + "&apikey=" + apikey
+// 				+ "&outputMode=" + outputMode + "&sourceText=" + sourceText
+// 				+ "&showSourceText=" + showSourceText, true);
+//
+// 		req.addEventListener("load", function(){
+// 			if (!(req.status >= 200 && req.status < 400)) {
+// 				console.log("Error in network request" + req.statusText);
+// 			} else {
+// 				var response = JSON.parse(req.responseText);
+// 				if(response.status == "ERROR"){
+// 					console.log(response.statusInfo);
+// 				}
+// 				else {
+// 					var angerFloat = parseFloat(response.docEmotions.anger);
+// 					var disgustFloat = parseFloat(response.docEmotions.disgust);
+// 					var fearFloat = parseFloat(response.docEmotions.fear);
+// 					var joyFloat = parseFloat(response.docEmotions.joy);
+// 					var sadnessFloat = parseFloat(response.docEmotions.sadness);
+// 					var unknownFloat = Math.abs(1.0 - (angerFloat + disgustFloat + fearFloat + joyFloat + sadnessFloat));
+//
+//           document.getElementById("sourceURL").textContent = response.url;
+// 					document.getElementById("sourceText").textContent = response.text;
+// 					document.getElementById("anger").textContent = response.docEmotions.anger;
+// 					document.getElementById("disgust").textContent = response.docEmotions.disgust;
+// 					document.getElementById("fear").textContent = response.docEmotions.fear;
+// 					document.getElementById("joy").textContent = response.docEmotions.joy;
+// 					document.getElementById("sadness").textContent = response.docEmotions.sadness;
+//
+// 				// Doughnut Chart Options
+// 				var doughnutOptions = {
+// 					//Boolean - Whether we should show a stroke on each segment
+// 					segmentShowStroke : true,
+//
+// 					//String - The colour of each segment stroke
+// 					segmentStrokeColor : "#fff",
+//
+// 					//Number - The width of each segment stroke
+// 					segmentStrokeWidth : 2,
+//
+// 					//The percentage of the chart that we cut out of the middle.
+// 					percentageInnerCutout : 40,
+//
+// 					//Boolean - Whether we should animate the chart
+// 					animation : true,
+//
+// 					//Number - Amount of animation steps
+// 					animationSteps : 75,
+//
+// 					//String - Animation easing effect
+// 					animationEasing : "easeOutBounce",
+//
+// 					//Boolean - Whether we animate the rotation of the Doughnut
+// 					animateRotate : true,
+//
+// 					//Boolean - Whether we animate scaling the Doughnut from the centre
+// 					animateScale : true,
+//
+// 					//Function - Will fire on animation completion.
+// 					onAnimationComplete : null,
+//
+// 					//Legend - rounded
+// 					tooltipTemplate : function(v) {return +(100 * v.value).toFixed(2);}
+// 				}
+//
+// 				// Doughnut Chart Data
+// 				var doughnutData = [
+// 					{
+// 						value : angerFloat,
+// 						color : "#CB4B16",
+// 						label : "Anger"
+// 					},
+// 					{
+// 						value : disgustFloat,
+// 						color : "#1F8261",
+// 						label : "Disgust"
+// 					},
+// 					{
+// 						value: fearFloat,
+// 						color: "white",
+// 						label : "Fear"
+// 					},
+// 					{
+// 						value : joyFloat,
+// 						color : "#FFA500",
+// 						label : "Joy"
+// 					},
+// 					{
+// 						value : sadnessFloat,
+// 						color : "#1789D4",
+// 						label : "Sadness"
+// 					},
+// 					{
+// 						value : unknownFloat,
+// 						color : "grey",
+// 						label : "Unknown"
+// 					}
+//
+// 				]
+//
+// 				//Get the context of the Doughnut Chart canvas element we want to select
+//
+// 				var ctx = document.getElementById("doughnutChart").getContext("2d");
+// 				ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+//
+// 				// Create the Doughnut Chart
+// 				// Prevent Chart Resize by Destorying Old One
+// 				if(mydoughnutChart != null){
+// 					mydoughnutChart.destroy();
+// 				}
+//
+// 				mydoughnutChart = new Chart(ctx).Doughnut(doughnutData, doughnutOptions);
+//
+// 				// Create Legend
+// 				document.getElementById("js-legend").innerHTML = mydoughnutChart.generateLegend();
+// 				}
+// 	}});
+// 	req.send(null);
+// 	event.preventDefault();
+// 	});
+// }
+
+
+
+
+// //Random User
+// $.ajax({
+//   url: 'https://randomuser.me/api/',
+//   dataType: 'json',
+//   success: function(data) {
+//     console.log(data);
+//   }
+// });
+
+//Giphy
+ //
+ // $.ajax({
+ //  url: 'http://api.giphy.com/v1/gifs/search?q=' + postTitle + '&api_key=dc6zaTOxFJmzC',
+ //  dataType: 'json',
+ //  success: function(response) {
+ //    // console.log(response.data);
+ //    var resArr = response.data;
+ //    var randomItem = resArr[Math.floor(Math.random()*resArr.length)];
+ //
+ //    giphUrl = randomItem.images.downsized_small.mp4;
+ //    // resArr.forEach(function(i){
+ //    //   //  console.log(i);
+ //    // })
+ //   }
+ //  });
 
 
 
--- emotion-reader/server/main.js
+++ emotion-reader/server/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/items.js';
+var sadness;
+var joy;
+var fear;
+var disgust;
+var anger;
 
 
 Meteor.startup(() => {
@@ -21,7 +26,7 @@ Meteor.startup(() => {
          Posts.insert({
            title: posts[i].title,
            text: posts[i].text,
-           published: posts[i].published,
+           published: posts[i].published
          });
        }
      }
@@ -34,4 +39,53 @@ Meteor.startup(() => {
 //   return user;
 // });
 
+
+'use strict';
+const fs = require('fs');
+const path = require('path')
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+// require('dotenv').config({ silent: true }); //  optional
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  // note: if unspecified here, credentials are pulled from environment properties:
+  // NATURAL_LANGUAGE_UNDERSTANDING_USERNAME &  NATURAL_LANGUAGE_UNDERSTANDING_PASSWORD
+  username: "",
+  password: "",
+  version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2016_01_23
 });
+
+const filename = Posts.findOne().text;
+console.log(filename);
+    const options = {
+      // html: file_datca,
+      text: filename,
+
+        features: {
+          entities: {
+            emotion: true,
+            sentiment: true
+          },
+        concepts: {},
+        keywords: {
+          emotion: true,
+          sentiment: true
+        }
+      }
+    };
+    nlu.analyze(options, function(err, res) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      sadness = res.keywords[0].emotion.sadness;
+      joy = res.keywords[0].emotion.joy;
+      fear = res.keywords[0].emotion.fear;
+      disgust = res.keywords[0].emotion.disgust;
+      anger = res.keywords[0].emotion.anger;
+
+      console.log(sadness, joy, fear, disgust, anger);
+    });
+ });
+
+// });
